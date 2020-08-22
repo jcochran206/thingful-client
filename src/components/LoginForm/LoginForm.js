6949fcb82af408ee47a73728aa30020421a60f35
Jonathan O Cochran <jonathan.o.cochran@gmail.com>
2020-08-22 13:47:58 -0700

    made basic auth changes for login form and service in app, form, n service

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
+import TokenService from '../../services/token-service'
 
 export default class LoginForm extends Component {
   static defaultProps = {
@@ -11,6 +12,11 @@ export default class LoginForm extends Component {
   handleSubmitBasicAuth = ev => {
     ev.preventDefault()
     const { user_name, password } = ev.target
+
+    //added for token auth 
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(user_name.value, password.value)
+    )
 
     console.log('login form submitted')
     console.log({ user_name, password })
--- src/services/thing-api-service.js
+++ src/services/thing-api-service.js
@@ -1,9 +1,11 @@
 import config from '../config'
+import TokenService from './token-service'
 
 const ThingApiService = {
   getThings() {
     return fetch(`${config.API_ENDPOINT}/things`, {
-      headers: {
+      headers: { //authorization header to basic here
+        'authorization': `basic ${TokenService.getAuthToken()}`
       },
     })
       .then(res =>
@@ -26,6 +28,7 @@ const ThingApiService = {
   getThingReviews(thingId) {
     return fetch(`${config.API_ENDPOINT}/things/${thingId}/reviews`, {
       headers: {
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
     })
       .then(res =>
@@ -39,6 +42,7 @@ const ThingApiService = {
       method: 'POST',
       headers: {
         'content-type': 'application/json',
+        'authorization': `basic ${TokenService.getAuthToken()}`,
       },
       body: JSON.stringify({
         thing_id: thingId,
